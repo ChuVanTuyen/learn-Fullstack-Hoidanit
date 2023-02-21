9d77d53bde4b8097085eb3b28fbabd5d6630c03c
Chu Van Tuyen <chuvantuyen512@gmail.com>
2023-02-22 00:30:10 +0700

    video 30

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,15 @@
 
-let getHomePage = (req, res) => {
+import db from "../models/index";
+
+let getHomePage = async (req, res) => {
     //return res.send("hello world from controller");//bắn 1 dòng chữ
-    return res.render('homePage.ejs');//render 1 file html ejs
+    try {
+        let data = await db.user.findAll(); // lệnh tìm kiếm tất cả các data có trong bảng user mysql bằng sequelize
+        return res.render('homePage.ejs');//render 1 file html ejs
+    } catch (e) {
+        console.log(e);
+    }
+
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
 require("dotenv").config();// phải có để dùng được process.env.PORT
 
 let app = express();
@@ -13,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);//kết nối với view engine
 initWebRoutes(app);//khởi tạo route
+
+connectDB(); //kết nối đến database
 
 let port = process.env.PORT || 8088;//lấy port từ file .env
 app.listen(port, () => {
