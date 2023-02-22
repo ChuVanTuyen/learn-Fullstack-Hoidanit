ebc81b472026eec64d751849044d876eab295c98
Chu Van Tuyen <chuvantuyen512@gmail.com>
2023-02-22 23:56:31 +0700

    video 32

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,8 +4,10 @@ import db from "../models/index";
 let getHomePage = async (req, res) => {
     //return res.send("hello world from controller");//bắn 1 dòng chữ
     try {
-        let data = await db.user.findAll(); // lệnh tìm kiếm tất cả các data có trong bảng user mysql bằng sequelize
-        return res.render('homePage.ejs');//render 1 file html ejs
+        let data = await db.User.findAll(); // lệnh tìm kiếm tất cả các data có trong bảng user mysql bằng sequelize
+        return res.render('homePage.ejs', {
+            data: JSON.stringify(data)
+        });//render 1 file html ejs
     } catch (e) {
         console.log(e);
     }
--- src/migrations/spectialty-migrations.js
+++ src/migrations/spectialty-migrations.js
@@ -2,21 +2,18 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('specialties', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      firstName: {
+      image: {
         type: Sequelize.STRING
       },
-      lastName: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
+      desciption: {
+        type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
@@ -29,6 +26,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('specialties');
   }
 };
